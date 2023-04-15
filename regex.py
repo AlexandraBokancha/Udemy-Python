a6434111569839be3df3584bc0d39398b7e627fb
Alexandra Bokancha <125573798+AlexandraBokancha@users.noreply.github.com>
2023-04-15 22:40:40 +0200

    Update regex.py

--- regex.py
+++ regex.py
@@ -12,7 +12,6 @@ def compare_patterns(regex_pattern, input_string):
     """Compare two equal length strings.
     Regex can contain different operators ('?', '*', '+') that control
     repetition of a character within a string"""
-
     if regex_pattern and regex_pattern[0] == "\\":
         regex_pattern = regex_pattern[1:]
     if not regex_pattern or regex_pattern == '$' and not input_string:
@@ -25,7 +24,7 @@ def compare_patterns(regex_pattern, input_string):
     elif len(regex_pattern) > 1 and regex_pattern[1] == "+" and input_string:
         return compare_patterns(regex_pattern[0] + regex_pattern[2:], input_string) or compare_patterns(regex_pattern,
                                                                                                         input_string[
-                                                                                                        1:])
+                                                                                                       1:])
     elif not input_string or not compare_regex_to_input(regex_pattern[0], input_string[0]):
         return False
     else:
@@ -34,7 +33,6 @@ def compare_patterns(regex_pattern, input_string):
 
 def compare_str(regex_pattern, string):
     """Compare two strings using a recursion"""
-
     if not string and regex_pattern:
         return False
     elif compare_patterns(regex_pattern, string):
